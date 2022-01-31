82af669c5ed5ae9052d2861ec149db5097813795
J Carlos <jhomanidev@gmail.com>
2022-01-31 10:41:33 -0400

    finish backend docker

---
+++ frontend/start.sh
@@ -0,0 +1,16 @@
+#!/bin/sh
+
+ssh="$HOME/.ssh";
+baseRute="$HOME/projects";
+project="alcaldia";
+
+read -p "Proyect Name: ($project) " project
+
+if [ ! $project ]
+then
+	project='alcaldia' 
+fi;
+
+echo $project $1 $2 $3
+
+eval "docker run -it --rm -p 3001:3001 -v $ssh:/home/admin/.ssh -v $baseRute/$project:/home/admin/app alcaldia $1 $2 $3";
